cf5d486fe44f68ac206372f8ebe35b6bed5c4424
Nguyen Nhat <quocnhat1503@gmail.com>
2022-07-13 07:57:40 +0700

    Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  USER_ATTR = %i(name email password password_confirmation).freeze
   before_save :downcase_email
 
   validates :name, presence: true, length: { maximum: Settings.user.name_max }
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
     root "static_pages#home"
-    get "/help", to: "static_pages#help"
-    get "/about", to: "static_pages#about"
-    get "/contact", to: "static_pages#contact"
-    get "/signup", to: "user#new"
+    resources :users, only: [:new, :show, :create]
+    get "/help", to:"static_pages#help"
+    get "/about", to:"static_pages#about"
+    get "/contact", to:"static_pages#contact"
+    get "/signup", to: "users#new"
   end
 end
